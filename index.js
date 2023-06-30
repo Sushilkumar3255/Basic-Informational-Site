d1270a587beb3fddc06ef39b66f110f60f0e1b2b
Sushil3456 <sushil.kumar@solytics-partners.com>
2023-06-30 12:26:05 +0530

    Basic Informational Site

---
+++ index.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const fs = require("fs");
+
+const port = 5500;
+const hostname = "127.0.0.1";
+
+const server = http.createServer((req, res) => {
+    switch(req.url) {
+        case "/":
+            fs.readFile(__dirname+"/home.html", (err, data) => {
+                if(err) {
+                    throw err;
+                }
+
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.write(data);
+                return res.end();
+            });
+            break;
+        case "/about":
+            fs.readFile(__dirname+"/about.html", (err, data) => {
+                if(err) {
+                    throw err;
+                }
+
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.write(data);
+                return res.end();
+            });
+            break;
+        case "/contact":
+            fs.readFile(__dirname+"/contact.html", (err, data) => {
+                if(err) {
+                    throw err;
+                }
+
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.write(data);
+                return res.end();
+            });
+            break;
+        default:
+            fs.readFile(__dirname+"/404.html", (err, data) => {
+                if(err) {
+                    throw err;
+                }
+
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.write(data);
+                return res.end();
+            });
+            break;
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
